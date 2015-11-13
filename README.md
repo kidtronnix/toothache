5749129691eb8e4eda26d5d9b1c406836bd87d8f
Simon Maxwell-Stewart <smaxwellstewart@gmail.com>
2015-11-13 13:05:27 -0500

    updated mongo deps and bumped to v2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Toothache
 
 A Hapi plugin that removes the toothache from creating CRUD endpoints for MongoDB.
 
-Current version: **1.1.x** [![Build Status](https://travis-ci.org/smaxwellstewart/toothache.svg?branch=master)](https://travis-ci.org/smaxwellstewart/toothache) [![Coverage Status](https://img.shields.io/coveralls/smaxwellstewart/toothache.svg)](https://coveralls.io/r/smaxwellstewart/toothache?branch=master)
+Current version: **2.0.x** [![Build Status](https://travis-ci.org/smaxwellstewart/toothache.svg?branch=master)](https://travis-ci.org/smaxwellstewart/toothache) [![Coverage Status](https://img.shields.io/coveralls/smaxwellstewart/toothache.svg)](https://coveralls.io/r/smaxwellstewart/toothache?branch=master)
 
 ## What is this plugin?
 
@@ -19,7 +19,7 @@ The below is intended to be added into a hapi plugin. In our example case, we wi
 
 ### Configure
 
-Configure toothache with desired behaviour... 
+Configure toothache with desired behaviour...
 
 ```js
 // User model
@@ -28,26 +28,26 @@ var CRUD = {
     collection: 'users',    // MongoDB collection
     // Create options
     create: {
-        // Valid create payload 
-        payload: Joi.object().keys({ 
+        // Valid create payload
+        payload: Joi.object().keys({
             email: Joi.string().required(),
             password: Joi.string().required()
         }),                 
         defaults: {         // Default values that will be added at doc creation
             access: 'normal',
             activated: false,
-            uId: true       // Field used for access control. This is a special field that when set to true will default to user's id 
+            uId: true       // Field used for access control. This is a special field that when set to true will default to user's id
                             // The value comes from, 'request.auth.artifacts.id' ie the id the user authenticates with
         },
         bcrypt: 'password', // Sets 'password' field to be bcrypted at doc creation
         date: 'created',    // Sets 'created' field to be dated at doc creation
-        access: "admin"     // Sets which role can create 
+        access: "admin"     // Sets which role can create
     },
     // Read options for get and find
     read: {
-        whitelist: ['email'],   // Array of fields that will be returned, all other fields will be excluded 
+        whitelist: ['email'],   // Array of fields that will be returned, all other fields will be excluded
         blacklist: ['password'], // Array of fields that will be removed, all other fields will be included
-        access: 'normal'        // Sets which role can read 
+        access: 'normal'        // Sets which role can read
     },
     // Update options
     update: {
@@ -55,20 +55,20 @@ var CRUD = {
         payload: Joi.object().keys({
             email: Joi.string(),
             password: Joi.string()
-        }), 
+        }),
         bcrypt: 'password', // Sets 'password' field to be bcrypted at doc update
         date: 'updated',    // Sets 'updated' field to be dated at doc update
         access: 'normal' // Sets which role can update  
     },
     // Delete options
     del: {
-        access: 'normal' // Sets which role can update 
+        access: 'normal' // Sets which role can update
     },
     // Joi options when validating payloads    
     validationOpts: {
         abortEarly: false
     }
-    
+
 };
 
 var User = require('toothache')(CRUD);
@@ -82,10 +82,10 @@ Once we have configured toothache, the following request handlers will be expose
  - This handler will insert any supplied `payload` into MongoDB.
  - Accepted methods: `GET` with `payload` in URL or, `POST` or `PUT` with `payload` in request body.
  - The following toothache `options` will affect this handler:
-  - `db` - MongoDB connection object, connection [example](https://gist.github.com/smaxwellstewart/9cf26df20cb58a3f5d02). 
+  - `db` - MongoDB connection object, connection [example](https://gist.github.com/smaxwellstewart/9cf26df20cb58a3f5d02).
   - 'collection' - the MongoDB collection to create, read, update and delete from.
   - `create.payload` - [Joi](https://github.com/hapijs/joi) object payload is validated against.
-  - `create.defaults` - Object of default fields, the payload will extend this object before insertion, 
+  - `create.defaults` - Object of default fields, the payload will extend this object before insertion,
   e.g. supplied payload will join and override this default object.
   - `create.bcrypt` - Field name of `payload` field to be bcrypted before doc creation.
   - `create.date` - Will add a javasctipt `new Date()` timestamp to field name at doc creation.
@@ -115,13 +115,13 @@ Once we have configured toothache, the following request handlers will be expose
   e.g. supplied payload will join and override this default object.
   - `update.bcrypt` - Field name of `payload` field to be bcrypted when doc is updated.
   - `update.date` - Will add a javasctipt `new Date()` timestamp to field name when doc is updated.
-  - `update.access` - If set to `admin` only admin users will be able to update a doc. If set to normal, both admin and normal users have update access. 
+  - `update.access` - If set to `admin` only admin users will be able to update a doc. If set to normal, both admin and normal users have update access.
 
 #### `.del`
  - This route will delete a doc with any supplied `payload`.
  - Accepted methods: `DELETE` with an `id` parameter set in route's `path` field.
  - The following toothache `options` will affect this handler:
-  - `del.access` - If set to `admin` only admin users will be able to delete a doc. If set to normal, both admin and normal users have delete access. 
+  - `del.access` - If set to `admin` only admin users will be able to delete a doc. If set to normal, both admin and normal users have delete access.
 
 *Example*
 
@@ -207,4 +207,3 @@ getCredentialsFunc: function (id, callback) {
 
  - Admin users get access to all resources, they can create, read, update and delete.
  - Normal users only have access to their own resources, they can only CRUD documents that have a `uId` equal to user's authenitcation id (`request.auth.artifacts.id`)
-
--- index.js
+++ index.js
@@ -36,11 +36,11 @@ module.exports = function(config) {
     var db = config.db;
     // get mongo collection
     var coll = config.collection;
-    
+
     var CRUD = {
         create: function(request, reply) {
-            
-            
+
+
             // add access control
             // We need to stop create if not allowed, only if:
             // route is authenticated, we are not admin, and we protect create
@@ -56,16 +56,16 @@ module.exports = function(config) {
                 } else {
                     var payload = request.payload;
                 }
-                
+
                 // First validate schema
-                // respond with errors 
+                // respond with errors
                 Joi.validate(payload, validSchema, config.validationOpts, function (err, value) {
                     if(err) {
                         var error = Boom.badRequest(err);
-                        return reply(error);   
+                        return reply(error);
                     }
                     else {
-                        
+
                         // Add our defaults
                         var insert = Extend({},config.create.defaults, payload);
 
@@ -79,7 +79,7 @@ module.exports = function(config) {
                             // Hash password before insert
                             insert[config.create.bcrypt] = Bcrypt.hashSync(insert[config.create.bcrypt], salt);
                         }
-                        
+
 
                         // Add uId if set to anything in defaults
                         if(request.auth.isAuthenticated && config.create.defaults["uId"] !== undefined) {
@@ -91,15 +91,15 @@ module.exports = function(config) {
 
                         // Perform Insert
                         collection.insert(insert, function(err, docs) {
-                           
-                            return reply(docs[0]).type('application/json');           
-                        });         
+
+                            return reply(docs.ops[0]).type('application/json');           
+                        });
                     }
-                }); 
-            }   
+                });
+            }
         },
         get: function(request, reply) {
-            
+
             if(request.auth.isAuthenticated && request.auth.credentials.access !== 'admin' && request.auth.credentials.access !== config.read.access) {
                 var error = Boom.unauthorized('You do not have read access');
                 return reply(error);
@@ -108,8 +108,8 @@ module.exports = function(config) {
                 var collection = db
                 .collection(coll)
                 .findOne({"_id": ObjectId(request.params.id)}, function(err, doc) {
-                    
-                    
+
+
                     if(doc == null) {
                         var error = Boom.badRequest('No doc found in '+coll);
                         return reply(error);
@@ -122,7 +122,7 @@ module.exports = function(config) {
                     else {
 
                         if(config.read.whitelist || config.read.blacklist) {
-                            
+
                             if(config.read.whitelist) {
                                 // Add whitelist fields
                                 var _doc = {};
@@ -132,7 +132,7 @@ module.exports = function(config) {
                                         _doc[key] = doc[key];
                                     }
                                 }
-                                
+
                                 doc = _doc;
                             }
                             if(config.read.blacklist) {
@@ -144,11 +144,11 @@ module.exports = function(config) {
                             }
                         }
                         return reply(doc).type('application/json');
-                    }                    
+                    }
                 });
             }
-            
-            
+
+
         },
         find: function(request, reply) {
             // console.log(config.read.access)
@@ -169,7 +169,7 @@ module.exports = function(config) {
                 if(request.payload) {
                     Extend(find, payload);
                 }
-               
+
                 // Access Control
                 if(request.auth.isAuthenticated && request.auth.credentials.access !== 'admin') {
                     var uId = request.auth.artifacts.id;
@@ -177,7 +177,7 @@ module.exports = function(config) {
                 }
 
 
-                
+
                 var collection = db
                 .collection(coll)
                 .find(find)
@@ -186,40 +186,40 @@ module.exports = function(config) {
 
                     var _docs = [];
                     if(config.read.whitelist || config.read.blacklist) {
-                        
+
                         for(var i = 0; i < docs.length; i++) {
                             var doc = docs[i];
 
                             if(config.read.whitelist) {
                                 // Add whitelist fields
                                 var _doc = {}
-                                
+
                                 for(var j = 0; j < config.read.whitelist.length; j++) {
                                     var key = config.read.whitelist[j];
                                     if(doc[key] !== undefined) {
                                         _doc[key] = doc[key];
                                     }
-                                    
-                                    
+
+
                                 }
                                 _docs.push(_doc)
                             }
                             if(config.read.blacklist) {
                                 // Remove blacklist fields
-                                
+
                                 //console.log(config.read.blacklist.length)
                                 for(var j = 0; j < config.read.blacklist.length; j++) {
                                     var key = config.read.blacklist[j];
-                                    
+
                                     delete doc[key];
                                 }
                                 _docs.push(doc)
-                            }                
+                            }
                         }
                         docs = _docs;
-                        
+
                     }
-                    
+
                     return reply(docs).type('application/json');
                 });
             }
@@ -231,7 +231,7 @@ module.exports = function(config) {
             // console.log(config.update.access)
             if(request.auth.isAuthenticated && request.auth.credentials.access !== 'admin' && request.auth.credentials.access !== config.update.access) {
                 var error = Boom.unauthorized('You do not have update access');
-                
+
                 return reply(error);
             }
             else {
@@ -272,7 +272,7 @@ module.exports = function(config) {
                             if(doc === null) {
                                 var error = Boom.badRequest('No doc found in '+coll);
                                 return reply(error);
-                            } 
+                            }
                             // access control
                             else if(request.auth.isAuthenticated && request.auth.credentials.access !== 'admin' && doc.uId !== request.auth.artifacts.id) {
                                 var error = Boom.unauthorized('You are not permitted to update this');
@@ -280,15 +280,15 @@ module.exports = function(config) {
                             }
                             else {
                                 collection.update({"_id": ObjectId(resourceId)}, {$set: update}, {}, function(err, doc) {
-                                    
+
                                     return reply({error:null,message:'Updated successfully'});
                                 });
                             }
                         })
-                    }   
-                }); 
+                    }
+                });
             }
-            
+
         },
         del: function(request, reply) {
             if(request.auth.isAuthenticated && request.auth.credentials.access !== 'admin' && request.auth.credentials.access !== config.del.access) {
@@ -309,12 +309,12 @@ module.exports = function(config) {
                     }
                     else {
                         collection.remove( _del, function(err) {
-                           
-                            return reply({error:null,message:'Deleted successfully'});          
+
+                            return reply({error:null,message:'Deleted successfully'});
                         });
                     }
                 });
-            }        
+            }
         }
     };
     return CRUD;
